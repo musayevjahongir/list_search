87b5b6f795d0bd3a49f51148d545f2cef58871bc
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-14 09:05:27 +0500

    tig'indi

--- find03_max_min_sum.py
+++ find03_max_min_sum.py
@@ -5,4 +5,17 @@ def find_max_min_sum(data):
         data: list of numbers
     returns: sum of the maximum and minimum numbers in the list
     """
-    return 0
+    i=1
+    a=data[0]
+    while i<len (data):
+        if a<data[i]:
+            a=data[i]
+        i+=1
+    j=1
+    b=data[0]
+    while j<len (data):
+        if b>data[j]:
+            b=data[j]
+        j+=1
+    return a+b
+print(find_max_min_sum([1, 2, 3, 4, 5]))
