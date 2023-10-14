a2543102c46819905a2964addd48ce27fdc604cb
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-14 09:13:48 +0500

    katta juft sonni topish

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,4 +5,12 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    return 0
+    i=0
+    a=0
+    while i<len (data):
+        if a<data[i]:
+            if data[i]%2==0:
+                a=data[i]
+        i+=1
+    return a
+print(find_max_even([1, 4, 3, 8, 5]))
