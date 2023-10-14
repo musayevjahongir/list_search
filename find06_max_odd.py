77f41a20c1136fa1c75f0b4088f5245dda6f7ed3
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-14 09:20:45 +0500

    katta toq son

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,15 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    i=0
+    a=0
+    while i<len (data):
+        if a<data[i]:
+            if data[i]%2:
+                a=data[i]
+        i+=1
+    if a:
+        return a
+    else:
+        return -1
+print(find_max_odd([11, 7, 5, 4, 9]))
